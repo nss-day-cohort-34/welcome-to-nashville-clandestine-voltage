930cff2a23ae7aa0afcd11be2995443d682569e4
Brian Wilson <wilsonedit@gmail.com>
2019-07-30 14:12:44 -0500

    modified branch files to match master

--- parks/factory.js
+++ parks/factory.js
@@ -1,23 +1,71 @@
 
-const createParkHTML = (park) => {
-    const parkAddress = park.mapped_location.human_address
+//Functions that create the HTML string for RESULTS, which will be added to the DOM
+
+
+const createResultHTML = (restaurants) => {
     return `
-        <section class="parks">
-            <h1>${park.park_name}</h1>
-            <h2>${parkAddress.replace('{"address": "', "").replace('", "city": "', ", ").replace('", "state": "', ", ").replace('", "zip": ""}', "").replace('{"address": "', "").replace('", "city": "', ", ").replace('", "state": "', ", ").replace('", "zip": ""}', "").replace('", "zip": "37206"}', "")
-            .replace('", "zip": "37080"}', "").replace('", "zip": "37013"}', "").replace('", "zip": "37138"}', "").replace('", "zip": "37211"}', "")
-            .replace('", "zip": "37212"}', "").replace('", "zip": "37205"}', "").replace('", "zip": "37027"}', "").replace('", "zip": "37214"}', "")
-            .replace('", "zip": "37209"}', "").replace('", "zip": "37076"}', "").replace('", "zip": "37115"}', "")}</h2>
+        <section class="results__section">
+            <h3>${restaurants.name}</h3>
+            <h4>${restaurants.address}</h4>
+            <button type="button" id="foodSaveButton">Save to Itinerary</button>
+        </section>
+    `
+}
+
+
+//address from Metro Parks object was returned as a string that LOOKED like it was an object, but wasnt'. We did not want to print to the DOM the "object". Below uses a series of .replace to target the individual characters needed to be removed. Had to string together a series of .replace to make this work. 
+
+const createParkHTML = (park) => {
+    const parkAddress = JSON.parse(park.mapped_location.human_address)
+    return `
+        <section class="results__section parks">
+            <h3>${park.park_name}</h3>
+            <h4>${parkAddress.address}</h4>
             <button type="button" id="parkSaveButton">Save to Itinerary</button> 
         </section>
     `
 }
 
+const createResultsConcert = (concert) => {
+    return `
+        <section class="results__section">
+            <h3 class="results--name">${concert.name}</h3>
+            <h4 class="results--date">${concert.dates.start.localDate}</h4>
+            <p class="results--time">${concert.dates.start.localTime}</p>
+            <button class="concertSaveButton">Save to Itinerary</button>
+        </section>
+`
+}
+
+//Functions that create the HTML string for ITINERARY items, which will be added to the DOM
+
+const createItineraryRestaurant = (name, address) => {
+    return `
+    <section>
+        <h3>${name}</h3>
+        <h4>${address}</h4>
+    </section>
+`
+}
+
+
 const createItineraryPark = (name, address) => {
     return `
         <section>
-            <h2>${name}</h2>
-            <h3>${address}</h3>
+            <h3>${name}</h3>
+            <h4>${address}</h4>
+        </section>
+    `
+}
+
+
+
+const createItineraryConcert = (name, date, time) => {
+    return `
+        <section>
+            <h3>${name}</h3>
+            <h4>${date}</h4>
+            <p>${time}</p>
         </section>
     `
 }
--- parks/main.js
+++ parks/main.js
@@ -1,45 +1,112 @@
-const parkSearchButton = document.querySelector("#parkSearch")
-const parkSelect = document.querySelector('#parks_features')
-const parkSaveButton = document.querySelectorAll("#parkSaveButton")
-const parkResultsDOM = document.querySelector("#parkResults")
-const clearDeleteButton = document.querySelector(".clearParksButton")
+
+//Event listeners for search buttons, which invoke the fecth, factory and render results functions.
+
+restaurantSearchButton.addEventListener("click", () => {
+    getRestaurantData(restaurantSearch.value).then((results) => {
+        console.log(results.restaurants)
+        for (let item of results.restaurants) {
+            const foodHTML = createResultHTML(item)
+            renderRestaurant(foodHTML)
+        }
+        if (results.restaurants == 0 || results.restaurants == "undefined" || results.restaurants == "null") {
+            alert("Restaurant not found. Try another!")
+        }
+    })
+    
+})
 
 
 parkSearchButton.addEventListener("click", () => {
     getParkData(parkSelect.value)
-    .then((parks) => {
-       for (let park of parks) {
-            let parksHTML = createParkHTML(park)
-            renderPark(parksHTML)
-          
-        }
-    })
+        .then((parks) => {
+            for (let park of parks) {
+                let parksHTML = createParkHTML(park)
+                renderPark(parksHTML)
+
+            }
+        })
 })
+
+
+concertSearchButton.addEventListener("click", () => {
+    getConcertData(concertSelect.value)
+        .then(concerts => {
+            for (let concert of concerts._embedded.events) {
+                let concertHTML = createResultsConcert(concert)
+                renderConcert(concertHTML)
+            }
+
+        })
+})
+
+
+
+// Event listeners for save buttons, which use DOM traversal to get 
+// the inner text of the results HTML elements and print them to the DOM 
+// using the factory and render functions for the itinerary.
+
+
+restaurantResults.addEventListener("click", (event) => {
+    if (event.target.tagName === 'BUTTON') {
+        const button = event.target
+        const restaurantAddress = button.previousElementSibling
+        const restaurantName = restaurantAddress.previousElementSibling
+        let restaurantItineraryName = restaurantName.innerText
+        let restaurantItineraryAddress = restaurantAddress.innerText
+        const restaurantItineraryHTML = createItineraryRestaurant(restaurantItineraryName, restaurantItineraryAddress)
+        renderRestaurantItinerary(restaurantItineraryHTML)
+    }
+})
+
+
 
 parkResultsDOM.addEventListener("click", (event) => {
     if (event.target.tagName === 'BUTTON') {
         const button = event.target
         const parkAddress = button.previousElementSibling
         const parkName = parkAddress.previousElementSibling
-       
+
         let parkItineraryName = parkName.innerText
         let parkItineraryAddress = parkAddress.innerText
-       
+
         const parkItineraryHTML = createItineraryPark(parkItineraryName, parkItineraryAddress)
         renderParkItinerary(parkItineraryHTML)
     }
 })
 
-// const removeEl = document.querySelector("#thingToDelete")
-// const containerEl = document.querySelector("#container")
-// const deleteButton = document.querySelector("#deleteButton")
 
-// deleteButton.addEventListener("click", () => {
-//     containerEl.removeChild(removeEl)
-// })
+concertResultsInDOM.addEventListener("click", (event) => {
+    if (event.target.tagName === 'BUTTON') {
+        const button = event.target
+        const concertTime = button.previousElementSibling
+        const concertDate = concertTime.previousElementSibling
+        const concertName = concertDate.previousElementSibling
+        const concertItineraryName = concertName.innerText
+        const concertItineraryDate = concertDate.innerText
+        const concertItineraryTime = concertTime.innerText
+        const concertItineraryHTML = createItineraryConcert(concertItineraryName, concertItineraryDate, concertItineraryTime)
+        renderConcertItinerary(concertItineraryHTML)
+    }
+})
 
 
+//Clear results button event listener
+clearButton.addEventListener("click", () => {
 
+    concertResultsInDOM.innerText = ""
+    parkResultsDOM.innerText = ""
+    restaurantResults.innerText = ""
+})
 
+//Clear itinerary event listener
+clearItineraryButton.addEventListener("click", () => {
 
+    concertItineraryReference.innerText = ""
+    parkItineraryReference.innerText = ""
+    restaurantItineraryReference.innerText = ""
+})
 
+// print button
+document.getElementById("printButton").addEventListener("click", function () {
+    window.print();
+})
